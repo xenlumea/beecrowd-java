8aa5152961cc2b8d20a7a2734ef21390157b6726
Emanuel Costa Conrado <dev.conradoemanuelcosta@gmail.com>
2022-12-28 11:19:24 -0300

    Solução Bee1001;

---
+++ beecrowd-solucoes-java/src/Bee1001_ExtremamenteBasico.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Bee1001_ExtremamenteBasico {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int X, A, B;
+
+        A = input.nextInt();
+        B = input.nextInt();
+
+        X = A + B;
+
+        System.out.println("X = " + X);
+    }
+}
